e004a4496703dd29b4942afb9a8de53d30c9b98c
AntonFedoseev <fedoseev446@gmail.com>
2023-05-10 23:30:12 +0300

    main

--- src/main/java/org/example/ApplicationFlashcards.java
+++ src/main/java/org/example/ApplicationFlashcards.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 public class ApplicationFlashcards {
 
     public static void main(String[] args) {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+        dataSource.getUrl();
+        dataSource.getUser();
+        dataSource.getPassword();
+//
+//        try {
+//            Connection connection = dataSource.getConnection();
+//            Statement statement = connection.createStatement();
+//        }
+
 
     }
 }
