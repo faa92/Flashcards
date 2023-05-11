0704a9a8357b7ddeb8ac5a3011efe2e40037b9d8
AntonFedoseev <fedoseev446@gmail.com>
2023-05-11 23:03:19 +0300

    fix card

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -6,26 +6,23 @@ CREATE TABLE theme (
       theme_title      TEXT NOT NULL
 );
 
-CREATE TABLE sub_theme(
-    sub_theme_id        BIGSERIAL PRIMARY KEY,
-    sub_theme_title     TEXT NOT NULL,
-    theme_id            BIGINT       NOT NULL REFERENCES theme (theme_id)
-);
-
 CREATE TABLE card(
-    card_id             BIGSERIAL PRIMARY KEY,
-    question            TEXT NOT NULL,
-    answer              TEXT NOT NULL,
-    is_remember         BOOLEAN NOT NULL,
-    sub_theme_id        BIGINT NOT NULL REFERENCES sub_theme(sub_theme_id)
+                     card_id  BIGSERIAL PRIMARY KEY,
+                     theme_id BIGINT  NOT NULL REFERENCES theme ON DELETE CASCADE,
+                     question TEXT    NOT NULL,
+                     answer   TEXT    NOT NULL,
+                     learned  BOOLEAN NOT NULL
 );
 
-INSERT INTO theme (theme_title) VALUES ('ENGLISH');
-INSERT INTO theme (theme_title) VALUES ('IT');
-INSERT INTO theme (theme_title) VALUES ('HISTORY');
+INSERT INTO theme (theme_title)
+VALUES ('ENGLISH');
+INSERT INTO theme (theme_title)
+VALUES ('IT');
+INSERT INTO theme (theme_title)
+VALUES ('HISTORY');
 
-INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('COLOR', 1);
-INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('ANIMAL', 1);
-INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('VEGETABLES', 1);
+-- INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('COLOR', 1);
+-- INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('ANIMAL', 1);
+-- INSERT INTO sub_theme (sub_theme_title, theme_id) VALUES ('VEGETABLES', 1);
 
 
