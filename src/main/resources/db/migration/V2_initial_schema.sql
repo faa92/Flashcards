078b42fc158de4622163dc37798a9f7be07855e8
AntonFedoseev <fedoseev446@gmail.com>
2023-05-12 19:02:50 +0300

    add V2

---
+++ src/main/resources/db/migration/V2_initial_schema.sql
@@ -0,0 +1,39 @@
+INSERT INTO theme (theme_title)
+VALUES ('ENGLISH');
+INSERT INTO theme (theme_title)
+VALUES ('MATHEMATICS');
+INSERT INTO theme (theme_title)
+VALUES ('JAVA');
+
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (1, 'RED', 'КРАСНЫЙ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (1, 'GREEN', 'ЗЕЛЁНЫЙ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (1, 'BLUE', 'СИНИЙ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (1, 'WHITE', 'БЕЛЫЙ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (1, 'BLACK', 'ЧЁРНЫЙ', false);
+
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (2, '1*1', '1', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (2, '2*2', '4', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (2, '3*3', '9', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (2, '4*4', '16', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (2, '5*5', '25', false);
+
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (3, '\s', 'ПРОБЕЛЬНЫЙ СИМВОЛ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (3, '^', 'НАЧАЛО СТРОКИ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (3, '$', 'КОНЕЦ СТРОКИ', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (3, '\b', 'ГРАНИЦА СЛОВА', false);
+INSERT INTO card (theme_id, question, answer, learned)
+VALUES (3, '\.', 'ЭКРАНИРОВАНИЕ СПЕЦ.СИМВОЛОВ', false);
