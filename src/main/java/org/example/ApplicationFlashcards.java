2e450d507b2f6eec8e31e9e257f3b8432369c3bf
AntonFedoseev <fedoseev446@gmail.com>
2023-05-17 17:55:09 +0300

    add CardJdbcRepository

--- src/main/java/org/example/ApplicationFlashcards.java
+++ src/main/java/org/example/ApplicationFlashcards.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.example.repository.CardJdbcRepository;
+import org.example.repository.ThemeJdbcRepository;
 import org.postgresql.ds.PGSimpleDataSource;
+
+import java.util.Scanner;
 
 public class ApplicationFlashcards {
 
@@ -9,11 +13,31 @@ public class ApplicationFlashcards {
         dataSource.setUrl(System.getenv("FLASH_CARDS_URL"));
         dataSource.setUser(System.getenv("FLASH_CARDS_USER"));
         dataSource.setPassword(System.getenv("FLASH_CARDS_PASSWORD"));
-//
-//        try {
-//            Connection connection = dataSource.getConnection();
-//            Statement statement = connection.createStatement();
-//        }
+
+        ThemeJdbcRepository themeJdbcRepository = new ThemeJdbcRepository(dataSource);
+        CardJdbcRepository cardJdbcRepository = new CardJdbcRepository(dataSource);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                listThemes           <idTheme>
+                getTheme             <idTheme>
+                getLearnedCards      <idTheme> <idCard>
+                getNotLearnedCards   <idTheme> <idCard>
+                addTheme             <them> 
+                removeThem           <idTheme>
+                getCardsOfTheme      <idTheme> <idCard>
+                addCard              <idTheme> <idCard>
+                exit""");
+        while (true) {
+            System.out.println("> ");
+            String command = scanner.nextLine();
+            String[] parts = command.split("\\s+");
+            switch (parts[0]) {
+                case "listThemes" -> {
+
+                }
+            }
+        }
 
 
     }
